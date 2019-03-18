00913b37c813102735710766b0d71548a72887ba
dwijngaarden <donovan.wijngaarden@student.rocvf.nl>
2019-03-18 14:22:10 +0100

    hoofdstuk 10 opdracht4

--- src/h10/Opdracht4.java
+++ src/h10/Opdracht4.java
@@ -19,84 +19,95 @@ public class Opdracht4 extends Applet {
         maandlabel = new Label("typ een maandnummer in");
         add(maandlabel);
 
-        maandtekstvak = new TextField("",10);
+        maandtekstvak = new TextField("", 10);
         add(maandtekstvak);
 
         jaarlabel = new Label("typ een jaargetal in");
         add(jaarlabel);
 
-        jaartekstvak = new TextField("",10);
+        jaartekstvak = new TextField("", 10);
         jaartekstvak.addActionListener(new jaartekstvaklistener());
         add(jaartekstvak);
         tekst = "";
-
 
 
     }
 
 
     public void paint(Graphics g) {
-        g.drawString(tekst,50,50);
+        g.drawString(tekst, 50, 100);
+        g.drawString(s, 50, 150);
 
 
     }
-    class jaartekstvaklistener implements ActionListener{
+
+
+
+    class jaartekstvaklistener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            s = maandtekstvak.getText();
-            maand = Integer.parseInt(s);
+            s = jaartekstvak.getText();
+            jaar = Integer.parseInt(s);
+            jaartekstvak.setText("");
 
 
-            if (( (jaar % 4 == 0 && !(jaar % 100 == 0)) ||
-                    jaar % 400 == 0 ) ){
-                tekst = "Februari,29 dagen";
+
+                tekst = maandtekstvak.getText();
+                maand = Integer.parseInt(tekst);
+                maandtekstvak.setText("");
 
 
+                switch (maand) {
+                    case 1:
+                        tekst = "Januari, 31 dagen";
+                        break;
+                    case 2:
+                        if ((jaar % 4 == 0 && !(jaar % 100 == 0)) ||
+                                jaar % 400 == 0) {
+                            tekst = "Februari, 29 dagen";
+                        } else {
+                            tekst = "Februari, 28 dagen";
+                        }
+
+                        break;
+                    case 3:
+                        tekst = "Maart, 31 dagen";
+                        break;
+                    case 4:
+                        tekst = "April, 30 dagen";
+                        break;
+                    case 5:
+                        tekst = "Mei, 31 dagen";
+                        break;
+                    case 6:
+                        tekst = "Juni, 30 dagen";
+                        break;
+                    case 7:
+                        tekst = "Juli, 31 dagen";
+                        break;
+                    case 8:
+                        tekst = "Augustes, 31 dagen";
+                        break;
+                    case 9:
+                        tekst = "September, 30 dagen";
+                        break;
+                    case 10:
+                        tekst = "Oktober, 31 dagen";
+                        break;
+                    case 11:
+                        tekst = "November, 30 dagen";
+                        break;
+                    case 12:
+                        tekst = "December, 31 dagen";
+                        break;
+                    default:
+                        tekst = "typ een juist maandnummer in";
+                        break;
+
+
+                }
+                repaint();
             }
-
-            switch (maand){
-                case 1:
-                    tekst = "Januari, 31 dagen";
-                    break;
-                case 2:
-                    tekst = "Februari, 28 dagen";
-                    break;
-                case 3:
-                    tekst = "Maart, 31 dagen";
-                    break;
-                case 4:
-                    tekst = "April, 30 dagen";
-                    break;
-                case 5:
-                    tekst = "Mei, 31 dagen";
-                    break;
-                case 6:
-                    tekst = "Juni, 30 dagen";
-                    break;
-                case 7:
-                    tekst = "Juli, 31 dagen";
-                    break;
-                case 8:
-                    tekst = "Augustes, 31 dagen";
-                    break;
-                case 9:
-                    tekst = "September, 30 dagen";
-                    break;
-                case 10:
-                    tekst = "Oktober, 31 dagen";
-                    break;
-                case 11:
-                    tekst = "November, 30 dagen";
-                    break;
-                case 12:
-                    tekst = "December, 31 dagen";
-                    break;
-                default:
-                    tekst= "typ een juist maandnummer in";
-                    break;
-
-
-            }
-            repaint();
         }
     }
-}
+
+
