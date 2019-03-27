e67977d2c8d35760622b74f5226e1739aeee0e4a
dwijngaarden <donovan.wijngaarden@student.rocvf.nl>
2019-03-27 09:29:16 +0100

    hoofdstuk 11 opdracht1

---
+++ src/h11/Opdracht1.java
@@ -0,0 +1,25 @@
+package h11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdracht1 extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawLine(50,50,50,100);
+        g.drawLine(60,50,60,100);
+        g.drawLine(70,50,70,100);
+        g.drawLine(80,50,80,100);
+        g.drawLine(90,50,90,100);
+        g.drawLine(100,50,100,100);
+        g.drawLine(110,50,110,100);
+        g.drawLine(120,50,120,100);
+        g.drawLine(130,50,130,100);
+        g.drawLine(140,50,140,100);
+
+    }
+}
